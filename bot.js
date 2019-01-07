1ef710db2e951f633fa338e4682ecfdae7c799ab
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-07 23:06:13 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,61 +15,28 @@ bot.on("ready", async () => {
 
 module.exports.run = async (bot, message, args) => {
 
-  //!warn @daeshan <reason>
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("No can do pal!");
-  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.reply("Couldn't find them yo");
-  if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("They waaaay too kewl");
-  let reason = args.join(" ").slice(22);
+if(!warns.warns) {
+message.channel.send(new Discord.RichEmbed()
+.setDescription("There are no warnings")
+);
+return;
+};
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
+let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+if(!wUser) return message.reply("Couldn't find them yo");
+let warnlevel = warns[wUser.id].warns;
 
-  if(!warns[wUser.id]) warns[wUser.id] = {
-    warns: 0
-  };
-
-  warns[wUser.id].warns++;
-
-  fs.writeFile("./warnings.json", JSON.stringify(warns), (err) => {
-    if (err) console.log(err)
-  });
-
-  let warnEmbed = new Discord.RichEmbed()
-  .setDescription("Warns")
-  .setAuthor(message.author.username)
-  .setColor("#fc6400")
-  .addField("Warned User", `<@${wUser.id}>`)
-  .addField("Warned In", message.channel)
-  .addField("Number of Warnings", warns[wUser.id].warns)
-  .addField("Reason", reason);
-
-  let warnchannel = message.guild.channels.find(`name`, "logs1");
-  if(!warnchannel) return message.reply("Couldn't find channel");
-
-  warnchannel.send(warnEmbed);
-
-  if(warns[wUser.id].warns == 2){
-    let muterole = message.guild.roles.find(`name`, "muted");
-    if(!muterole) return message.reply("You should create that role dude.");
-
-    let mutetime = "10s";
-    await(wUser.addRole(muterole.id));
-    message.channel.send(`<@${wUser.id}> has been temporarily muted`);
-
-    setTimeout(function(){
-      wUser.removeRole(muterole.id)
-      message.reply(`<@${wUser.id}> has been unmuted.`)
-    }, ms(mutetime))
-  }
-  if(warns[wUser.id].warns == 3){
-    message.guild.member(wUser).ban(reason);
-    message.reply(`<@${wUser.id}> has been banned.`)
-  }
-
+message.channel.send(new Discord.RichEmbed()
+.setDescription("Warning Information")
+.setColor("#c40000")
+.addField("Member", <@${wUser.id}>)
+.addField("Amount of Warns", warnlevel)
+);
 }
 
 module.exports.help = {
-  name: "warn"
+name: "warnlevel"
 }
-
 
 bot.on("message", async message => {
 
