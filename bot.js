8b84fe2ab3099877b14a2c96bace04178e3ec98b
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-07 18:39:35 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+  
+  bot.user.setActivity("To my creator JustNela#666", {type: "LISTENING"});
+});
+
+
+
+
+
+
+bot.on("message", async message => {
+
+  if (message.author.bot) return;
+  if (message.channel.type === "dm") return;
+
+  let prefix = "!";
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  let a = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let b = args.join(" ").slice(22)
+  let logs = message.guild.channels.find(`name`, `logs`);
+  let mods = message.guild.roles.find("name", "Moderator");
+  let premium = message.guild.roles.find("name", "★†Premium†★");
+  
